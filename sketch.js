34293866903c4a62ea774e17340edd3b581b07b6
PYJS123 <73827861+PYJS123@users.noreply.github.com>
2021-03-02 16:03:04 +0000

    Fully made a node-scape

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
+let players = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  for (let i = 0; i < 15; i++) {
+    let w = random(40, 180);
+    let h = w;
+    players.push(new Player(random(width), random(height), w, h, color(random(255), random(255), random(255))));
+  }
+
+  rectMode(CENTER);
+  strokeWeight(10);
 }
 
 function draw() {
   background(51);
+
+  for (let i = 0; i < players.length; i++) {
+    players[i].update();
+    players[i].show();
+  }
 }
